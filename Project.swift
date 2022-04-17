6db51c1498787dae317d8e8aa762ba516bb55246
vasnev <26thapril1986@gmail.com>
2022-04-17 19:20:59 +0300

    wip

--- Project.swift
+++ Project.swift
@@ -1,4 +1,14 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
+extension Project {
+	public static func app(name: String) -> Project {
+		Project(
+			name: name,
+			organizationName: "CatFacts.io",
+			targets: Module.allCases.flatMap { $0.targets }
+		)
+	}
+}
+
 let project = Project.app(name: "CatFacts")
